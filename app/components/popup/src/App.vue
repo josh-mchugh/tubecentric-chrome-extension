260ebb0a2c794f1f2592aed713a1bdf9ea7dfc01
jmchugh <joshua.mchugh01@gmail.com>
2019-10-27 14:47:52 -0400

    Adding alias for fomantic back.

--- app/components/popup/src/App.vue
+++ app/components/popup/src/App.vue
@@ -14,7 +14,7 @@
   </div>
 </template>
 
-<style scoped src="../../../../fomantic/dist/semantic.min.css"></style>
 <style lang="scss">
-  @import "./styles/main.scss";
+  @import url("./styles/main.scss");
+  @import url('~fomantic/semantic.min.css');
 </style>
